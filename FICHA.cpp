21db7da43a26b1b0dc68847e4acd8ebd7f04d577
Erika Tamo Turpo <etamotu@gmail.com>
2016-05-02 23:15:36 -0500

    Update FICHA.cpp

--- FICHA.cpp
+++ FICHA.cpp
@@ -1,5 +1,16 @@
+/**
+  *@file FICHA.h
+  *@version 1.0
+  *@date 02/05/2016
+  *@autores Hermanos Tamos
+  *@title Funciones nodos llamados fichas
+  *@brief Funciones Elementales
+  */
 #include "FICHA.h"
-
+/**
+ * @brief constructor cuando la ficha es un número
+ * @param number1 para dar este valor a number(dato mienbro)
+ */
 FICHA::FICHA(double number1)
 {
     this -> number = number1;
@@ -8,6 +19,10 @@ FICHA::FICHA(double number1)
     this -> evaluado = false;
     this -> acumulate = number1;
 }
+/**
+ * @brief Constructor cuando la ficha es un operador es decir cuando es char
+ * @param operador1 para dar este valor a operador(dato miembro).
+ */
 FICHA::FICHA(char operador1)
 {
     this -> operador = operador1;
@@ -20,36 +35,57 @@ FICHA::~FICHA()
 {
     //dtor
 }
+/**
+ * @breif Obtiene acumulate
+ * @return Retorna el acumulate(dato mienbro)
+ */
 double  FICHA::getAcumula()
 {
     return acumulate;
 }
-
+/**
+ * @breif Obtiene es evaluado
+ * @return Retorna un booleno que me dice si esta evaluado.
+ */
 bool FICHA :: isEvaluado()
 {
     return evaluado;
 }
-
+/**
+ * @breif Si la ficha fuese un numero entonces me devolveria ese valor.
+ * @return Retorna un double que es number de la ficha.
+ */
 double FICHA:: getNumero()
 {
     return number;
 }
-
-double  FICHA::getOperador()
+/**
+ * @breif Si la ficha fuese tipo operador entonces me devolveria este valor.
+ * @return Retorna el operador.
+ */
+char FICHA::getOperador()
 {
     return operador;
 }
-
+/**
+ * @breif  Se pregunta si la ficha  es operador o si es numero.
+ * @return Retorna 1 si es numero y 2 si es operador.
+ */
 int FICHA:: getTipo()
 {
     return kind;
 }
 
+/**
+ * @breif Chanca en el valor acumulate el acumulado
+ */
 void FICHA::setAcumula(double acumulado)
 {
     this->acumulate = acumulado;
 }
-
+/**
+  @breif Chanca en el valor evaluado al evaluado
+  */
 void FICHA::setEvaluado(bool evaluado)
 {
     this->evaluado = evaluado;
